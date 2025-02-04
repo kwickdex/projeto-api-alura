1518344028506a169b6310155dad4dd25a0313bd
Enzo Finotti <comercialuzerboy@gmail.com>
2025-02-04 13:21:17 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,45 @@
+const containerVideos = document.querySelector(".videos__container");
+console.log(containerVideos)
+
+async function buscarEMostrarVideos() {   
+    try{
+        const busca = await fetch("http://localhost:3000/videos");
+        const videos = await busca.json();
+        videos.forEach(video => {
+            containerVideos.innerHTML += `
+                <li class="videos__item">
+                    <iframe src="${video.url}" title="${video.titulo}" frameborder="0"allowfullscreen></iframe>
+                    <div class="descricao-video">
+                        <img class="img-canal" src="${video.imagem}" alt="Logo do Canal">
+                        <h3 class="titulo-video">${video.titulo}</h3>
+                        <p class="titulo-canal">${video.descricao}</p>
+                     </div>
+                </li>
+            `;
+        })
+    }
+    catch(error){
+        console.error("Something is wrong!!!")
+    }
+}
+
+buscarEMostrarVideos();
+
+const barraDePesquisa = document.querySelector(".pesquisar__input");
+
+barraDePesquisa.addEventListener("input", filtrarPesquisa);
+
+function filtrarPesquisa(){
+    const videos = document.querySelectorAll(".videos__item");
+    if(barraDePesquisa != ""){
+        videos.forEach((video) => {
+            let titulo = video.querySelector(".titulo-video").textContent.toLowerCase();
+            let valorFiltro = barraDePesquisa.value.toLowerCase();
+
+            (!titulo.includes(valorFiltro)) ? video.style.display = "none" : video.style.display = "block";
+        })
+    } else {
+        video.style.display = "block";
+    }
+}
+
